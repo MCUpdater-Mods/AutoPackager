a1f21245cee9148ef3e84dde73b7c3f3d896f6bf
Scott M. Barbour <smbarbour@gmail.com>
2017-07-12 02:36:46 -0500

    Corrected language files

--- src/main/java/org/mcupdater/autopackager/BlockPackager.java
+++ src/main/java/org/mcupdater/autopackager/BlockPackager.java
@@ -112,7 +112,7 @@ public class BlockPackager extends BlockContainer
 
 		public ItemBlockPackager(Block block) {
 			super(block);
-			setUnlocalizedName("reconstructorblock");
+			setUnlocalizedName("packagerblock");
 			setRegistryName(AutoPackager.metadata.modId, "packagerblock");
 		}
 	}
